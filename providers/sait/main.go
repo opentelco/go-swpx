4d756715f9e620817b2e4b87238cea3361aceeb5
Mathias Ehrlin <mathias@liero.se>
2021-01-26 14:00:50 +0100

    added sait

---
+++ providers/sait/main.go
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2020. Liero AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/hashicorp/go-version"
+
+	"git.liero.se/opentelco/go-swpx/proto/go/core"
+	"git.liero.se/opentelco/go-swpx/proto/go/provider"
+	"git.liero.se/opentelco/go-swpx/shared"
+)
+
+var VERSION *version.Version
+var logger hclog.Logger
+
+const (
+	VERSION_BASE string = "1.0-beta"
+)
+
+var PROVIDER_NAME = "sait"
+
+func init() {
+	var err error
+	if VERSION, err = version.NewVersion(VERSION_BASE); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	logger = hclog.New(&hclog.LoggerOptions{
+		Name:  fmt.Sprintf("%s@%s", PROVIDER_NAME, VERSION.String()),
+		Level: hclog.Debug,
+		Color: hclog.AutoColor,
+	})
+
+}
+
+// Provider is the implementation of the GRPC
+type Provider struct {
+	logger hclog.Logger
+}
+
+func (g *Provider) Version() (string, error) {
+	return fmt.Sprintf("%s@%s", PROVIDER_NAME, VERSION.String()), nil
+}
+
+func (p *Provider) Name() (string, error) {
+	return PROVIDER_NAME, nil
+}
+
+func (p *Provider) PostHandler(ctx context.Context, request *core.Response) (*core.Response, error) {
+	p.logger.Named("post-handler").Debug("processing response", "changes", 0)
+	return request, nil
+}
+
+func (p *Provider) PreHandler(ctx context.Context, request *core.Request) (*core.Request, error) {
+	p.logger.Named("pre-handler").Debug("processing request")
+
+	if request.AccessId != "" {
+		access, ok := translationMap[request.AccessId]
+		if ok {
+			p.logger.Named("pre-handler").Info("found access ID on provider", "access_id", request.AccessId, "network_element", access.NetworkElement, "port", access.Interface)
+			request.Hostname = access.NetworkElement
+			request.Port = access.Interface
+		} else {
+			p.logger.Named("pre-handler").Warn("could not find access id on provider", "access_id", request.AccessId)
+			return nil, fmt.Errorf("access id was not found with selected provider")
+		}
+	} else {
+		p.logger.Named("pre-handler").Debug("access id is empty")
+	}
+
+	return request, nil
+}
+
+func (p *Provider) Setup(ctx context.Context, request *provider.SetupConfiguration) (*provider.SetupResponse, error) {
+
+	return &provider.SetupResponse{}, nil
+}
+
+func (p *Provider) GetConfiguration(ctx context.Context) (*shared.Configuration, error) {
+	return nil, nil
+}
+
+func main() {
+	prov := &Provider{logger: logger}
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			shared.PluginProviderKey: &shared.ProviderPlugin{
+				Impl: prov,
+			},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+		Logger:     logger,
+	})
+
+}
---
+++ providers/sait/object_translation.go
@@ -0,0 +1,82 @@
+package main
+
+type Access struct {
+	NetworkElement string
+	Interface      string
+	SNMPCommunity  string
+	Username       string
+	Password       string
+}
+
+var translationMap = map[string]Access{
+	"5f024967cf1c": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/1",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"e2a4a1da985e": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/2",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"83d636a256a6": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/3",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"455ee346b701": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/4",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"576655b914e7": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/5",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"abc21bf112c6": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/6",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"27c509a2c3a7": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/7",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"df70884ef103": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/8",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"2b9fc2048a8c": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/9",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+	"abb77d79ad5d": Access{
+		NetworkElement: "10.5.5.100",
+		Interface:      "GigabitEthernet0/0/10",
+		SNMPCommunity:  "xWTyZ9nA158ktJF2",
+		Username:       "root",
+		Password:       "qwerty1234",
+	},
+}
