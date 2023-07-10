fa679e9e833b5eb5b49d4eebe5f391e44245a6b4
Mathias Ehrlin <mathias.ehrlin@vx.se>
2023-07-11 00:05:01 +0200

    ..

--- fleet/stanza/worker.go
+++ fleet/stanza/worker.go
@@ -2,14 +2,23 @@ package stanza
 
 import (
 	"git.liero.se/opentelco/go-swpx/fleet/stanza/activities"
+	"git.liero.se/opentelco/go-swpx/fleet/stanza/workflows"
 	"git.liero.se/opentelco/go-swpx/proto/go/fleet/stanzapb"
 	"go.temporal.io/sdk/worker"
+	"go.temporal.io/sdk/workflow"
 )
 
 func (n *stanzaImpl) newWorker() worker.Worker {
 	w := worker.New(n.temporalClient, stanzapb.TaskQueue_TASK_QUEUE_FLEET_STANZA.String(), worker.Options{})
 
+	w.RegisterWorkflowWithOptions(
+		workflows.ApplyStanzaWorkflow,
+		workflow.RegisterOptions{
+			Name: "fleet.stanza.apply",
+		})
+
 	act := activities.New(n)
+
 	w.RegisterActivity(act)
 	return w
 }
